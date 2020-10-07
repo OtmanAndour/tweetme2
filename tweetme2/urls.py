33c136ae21ff25f6cf1f7c33990a9ee08c907ef7
Othman Andour <andour.otman@gmail.com>
2020-10-07 18:03:29 +0200

    Pushing project to github

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tweets.views import home, tweet_details, tweet_list_view, tweet_create_view
+from tweets.views import (
+    home,
+    tweet_details,
+    tweet_list_view,
+    tweet_create_view,
+    tweet_delete_view
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('tweets/<int:tweet_id>', tweet_details),
     path('tweets', tweet_list_view),
     path('create-tweet', tweet_create_view),
+    path('tweets/<int:tweet_id>', tweet_details),
+    path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from django.utils.http import is_safe_url
 
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
 from .serializers import TweetSerializer
@@ -19,21 +23,60 @@ def home(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
 
 
+'''
+    REST API views with Django Rest Framework
+'''
+
+
+@api_view(['POST'])  # http methos the client sends must be a post
+# @authentication_classes([SessionAuthentication, MyCustomAuth])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
-    '''
-    REST API create view with Django Rest Framework
-    '''
-    data = request.POST or None
-    serializer = TweetSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-    return JsonResponse({}, status=400)
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
+
+
+@api_view(['GET'])
+def tweet_list_view(request, *args, **kwargs):
+    query_set = Tweet.objects.all()
+    serializer = TweetSerializer(query_set, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def tweet_details(request, tweet_id, *args, **kwargs):
+    query_set = Tweet.objects.filter(id=tweet_id)
+    if not query_set.exists():
+        return Response({}, status=404)
+    obj = query_set.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data, status=200)
+
+
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated])
+def tweet_delete_view(request, tweet_id, *args, **kwargs):
+    query_set = Tweet.objects.filter(id=tweet_id)
+    if not query_set.exists():
+        return Response({}, status=404)
+    query_set = query_set.filter(user=request.user)
+    if not query_set.exists():
+        return Response({"message": "You cannot delete this tweet"}, status=401)
+    obj = query_set.first()
+    obj.delete()
+    return Response({"message": "Tweet removed"}, status=200)
+
+
+'''
+    Views with pure Django
+'''
 
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
-    '''
-    Create view with pure Django
-    '''
+
     user = request.user
     if not request.user.is_authenticated:
         user = None
@@ -45,7 +88,7 @@ def tweet_create_view_pure_django(request, *args, **kwargs):
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
-        obj.user = user  # Annon user
+        obj.user = user
         obj.save()
         if request.is_ajax():
             return JsonResponse(obj.serialize(), status=201)
@@ -58,7 +101,7 @@ def tweet_create_view_pure_django(request, *args, **kwargs):
     return render(request, 'components/form.html', context={"form": form})
 
 
-def tweet_list_view(request, *args, **kwargs):
+def tweet_list_view_pure_django(request, *args, **kwargs):
     """
     REST API view so it can be consumed by Javascript or React
     Return json data
@@ -72,7 +115,7 @@ def tweet_list_view(request, *args, **kwargs):
     return JsonResponse(data)
 
 
-def tweet_details(request, tweet_id, *args, **kwargs):
+def tweet_details_pure_django(request, tweet_id, *args, **kwargs):
     """
     REST API view so it can be consumed by Javascript or React
     Return json data
